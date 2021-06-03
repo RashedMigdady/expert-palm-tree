3127dbd7ff77d938afccdce8eb5b5204627cc202
RashedMigdady <migdadyrashed@gmail.com>
2021-06-03 17:37:11 +0300

    third commit

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
+  final String answerText ;
+  final Function()? x;
 
+
+
+  Answer(this.x , this.answerText);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Text(answerText ,style: TextStyle(fontSize: 23),),
+          onPressed:  x,),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,53 @@
 import 'package:flutter/material.dart';
 import 'question.dart';
+import 'answer.dart';
 
-
-
-main(){
+main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatefulWidget {
-
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
 
-  int _questionIndex = 0 ;
+  void answerQuestion(){
+    if (_questionIndex == 1)
+      _questionIndex = -1;
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print("chosen answer");
+    print(_questionIndex);
+  }
 
   final _question = [
     'What\'s your favorite Color?',
     'What\'s your favorite Animal?'
-
   ];
-
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Quiz App"),
+          appBar: AppBar(
+            title: Text("Quiz App"),
+          ),
+          body: Container(
+            //width: double.infinity,
+            child: Column(children: <Widget>[
+              Question(_question[_questionIndex]),
+              Answer(answerQuestion, "Answer 1"),
+              Answer(answerQuestion, "Answer 2"),
+              Answer(answerQuestion, "Answer 3")
 
-        ),
-        body: Container(
-          child : Question(_question[1]),
 
-        ),
-      ),
+            ]),
+          )),
     );
   }
 }
